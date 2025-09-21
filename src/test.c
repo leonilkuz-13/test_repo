cdd3fdccd19f9bb23b3203b0da50bd0318ae851f
Leonid <leonilfilippenkov@gmail.com>
2025-09-21 22:42:55 +0300

    .

--- src/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "test.h"
-
-int main(void) {
-    int a,b,t;
-    scanf("%d ",&a,&b,&t);
-    printf("%d ", сalc_rect(a,b,t));
-    return 0;
-}
--- src/testik.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-
-int calc_rect(int w, int h, int type) {
-    if (type == 1) return (w * h);
-    else return (w + h) * 2 ;
-}
