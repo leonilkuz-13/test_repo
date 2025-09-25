8f30d4999642c7e36e81a523c979e93629ceffb5
Leonid <leonilfilippenkov@gmail.com>
2025-09-25 11:39:53 +0300

    add homework

--- src/homework in C /1 /2/the number zero elemets in the array.c
+++ src/homework in C /1 /2/the number zero elemets in the array.c
@@ -3,7 +3,7 @@
 int main(void) {
     int n,cnt = 0;
     scanf("%d",&n);
-    int ar_r[n];
+    const int ar_r[n];
     for (int i = 0; i < sizeof(ar_r) / sizeof(*ar_r); i++)
         if (ar_r[i] == 0) cnt++;
     printf("%d",cnt);
