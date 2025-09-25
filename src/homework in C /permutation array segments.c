0a76103feabc09879303c20fefe86702aba00f82
Leonid <leonilfilippenkov@gmail.com>
2025-09-25 15:42:32 +0300

    add permutations array segments

--- src/homework in C /permutation array segments.c
+++ src/homework in C /permutation array segments.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
-
-
-
 int main(void) {
     int const n,m;
     scanf("%d %d",&n,&m);
     if(n <= m)
         printf("incorrect input");
     else {
-        int array[n + 1];
+        int array[n + 1]; /*если n - последний элемент, то всего элементов n + 1*/
         for(int i = 0; i < sizeof(array) / sizeof(*array); i++)
             array[i] = i + 1; /*массив вида {1, 2, ..., n, n + 1}*/
         for (int k = 0; k <= n - (m + 1); k++) {
@@ -22,3 +18,5 @@ int main(void) {
     }
     return 0;
 }
+
+/*в условии задачи идндексация с array[1] элемента, а в моем решении индексация с array[0] элемента. Т.е., если на входе m = 2, то будут передвинуты 3 элемента. Надеюсь, что я правильно понял задачу и это не ошибка.*/
