2d18be23fd5dccc2ff2488d03729ab4066f8aad0
Leonid <leonilfilippenkov@gmail.com>
2025-09-25 15:40:56 +0300

    add permutation array segments

---
+++ src/homework in C /permutation array segments.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+
+int main(void) {
+    int const n,m;
+    scanf("%d %d",&n,&m);
+    if(n <= m)
+        printf("incorrect input");
+    else {
+        int array[n + 1];
+        for(int i = 0; i < sizeof(array) / sizeof(*array); i++)
+            array[i] = i + 1; /*массив вида {1, 2, ..., n, n + 1}*/
+        for (int k = 0; k <= n - (m + 1); k++) {
+            int t = array[n];
+            for (int j = n; j > 0; j--) 
+                array[j] = array[j - 1];
+            array[0] = t;
+        }
+        for(int i = 0; i < sizeof(array) / sizeof(*array); i++) printf("%d ",array[i]);
+    }
+    return 0;
+}
