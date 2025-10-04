3955693b53f689e236a10758ace38ba244cef460
Leonid <leonilfilippenkov@gmail.com>
2025-10-05 01:19:01 +0300

    add quick_sort

--- src/quick_sort_/quick_sort.c
+++ src/quick_sort_/quick_sort.c
@@ -6,23 +6,28 @@
 int arr[MAX_SIZE] = {};
 int lenArray = 0;
 
-void reading(void) {
+void reading(void)
+{
     while (scanf("%d ", arr + lenArray) == 1)
         lenArray++;
 }
 
-int distribution(int arr[], int low, int high) {
+int distribution(int arr[], int low, int high)
+{
     int pivot = arr[(low + high) / 2];
     int left = low - 1;
     int right = high + 1;
     int t;
 
-    while (true) {
-        do {
+    while (true)
+    {
+        do
+        {
             left++;
         } while (arr[left] < pivot);
 
-        do {
+        do
+        {
             right--;
         } while (arr[right] > pivot);
 
@@ -35,15 +40,18 @@ int distribution(int arr[], int low, int high) {
     }
 }
 
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
         int p = distribution(arr, low, high);
         quickSort(arr, low, p);
         quickSort(arr, p + 1, high);
     }
 }
 
-int main(void) {
+int main(void)
+{
     reading();
 
     int array1[MAX_SIZE];
@@ -53,7 +61,8 @@ int main(void) {
     quickSort(arr, 0, lenArray - 1);
 
     int count = 0;
-    for (int i = 0; i < lenArray; i++) {
+    for (int i = 0; i < lenArray; i++)
+    {
         if (arr[i] != array1[i])
             count++;
     }
---
+++ src/quick_sort_/readme.mc
@@ -0,0 +1,2 @@
+gcc -S quick_sort.c -o quick_sort.s - из исходного кода в асемблерный
+gcc -c quick_sort.s -o quick_sort - из асемблерного кода в бинарный файл
