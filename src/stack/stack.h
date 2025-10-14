f1a2400cbd6f2f15d68fdb6d594f8b4f8d74cf38
Leonid <leonilfilippenkov@gmail.com>
2025-10-14 21:18:06 +0300

    add stack

---
+++ src/stack/stack.h
@@ -0,0 +1,14 @@
+#ifndef STACK_H
+#define STACK_H
+
+typedef struct object_stack
+{
+    char symbol;
+    struct object_stack* last;
+} OBJ;
+
+OBJ* push(OBJ* top, char symbol);
+OBJ* pop(OBJ* top);
+void freestack(OBJ* top);
+
+#endif
