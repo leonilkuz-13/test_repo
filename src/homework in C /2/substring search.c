ff3446d9fc2e937cf353251ff8ffb6041615cbed
Leonid <leonilfilippenkov@gmail.com>
2025-11-17 19:27:47 +0200

    add corrected substring search

--- src/homework in C /2/substring search.c
+++ src/homework in C /2/substring search.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-/*в условии задачи не было указано количество символов во входном потоке, потому пользователь сможет ввести только 100 символов*/
+/*в условии задачи не было указано количество символов во входном потоке, потому
+ * пользователь сможет ввести только 100 символов*/
 
 int main(void)
 {
@@ -9,31 +10,26 @@ int main(void)
     char substr[100];
     int count = 0;
 
-    fgets(str,sizeof(str) - 1,stdin);
-    fgets(substr,sizeof(substr) - 1,stdin);
-    
-    char *g = strchr(str,'\n'), *g1 = strchr(substr,'\n');
+    fgets(str, sizeof(str) - 1, stdin);
+    fgets(substr, sizeof(substr) - 1, stdin);
 
-    if (g != NULL && g1 != NULL)
-    {
+    char *g = strchr(str, '\n'), *g1 = strchr(substr, '\n');
+
+    if (g != NULL && g1 != NULL) {
         *g = '\0';
         *g1 = '\0';
     }
 
-    char *ptr = str;
-    ptr = strstr(ptr,substr);
-    while (ptr != NULL) 
-    {
+    char* ptr = str;
+    ptr = strstr(ptr, substr);
+    while (ptr != NULL) {
         count++;
         ptr++;
-        ptr = strstr(ptr,substr);
+        ptr = strstr(ptr, substr);
     }
-    if (count > 0)
-    {
-        printf("count substr in str = %d",count);
-    }
-    else
-    {
+    if (count > 0) {
+        printf("count substr in str = %d", count);
+    } else {
         puts("The substr isn't in str");
     }
     return 0;
