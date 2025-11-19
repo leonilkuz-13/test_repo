923ec117606967a9e673fc9da86401859e6d4b51
Leonid <leonilfilippenkov@gmail.com>
2025-11-19 08:56:51 +0200

    add lucky tickets

--- src/homework in C /1/lucky tickets.c
+++ src/homework in C /1/lucky tickets.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    long int cnt = 0;
-    for (int i_1 = 0; i_1 <= 9; i_1++)
-    {
-        for (int i_2 = 0; i_2 <= 9; i_2++)
-        {
-            for (int i_3 = 0; i_3 <= 9; i_3++)
-            {
-                for (int i_4 = 0; i_4 <= 9; i_4++)
-                {
-                    for (int i_5 = 0; i_5 <= 9; i_5++)
-                    {
-                        for (int i_6 = 0; i_6 <= 9; i_6++)
-                        {
-                            if ((i_1 + i_2 + i_3) == (i_4 + i_5 + i_6))
-                                cnt++;
-                        }
-                    }
-                }
-            }
-        }
+    int count[28] = {0};
+    for (int n = 0; n < 1000; n++) {
+        int sum = (n / 100) + ((n / 10) % 10) + (n % 10);
+        count[sum]++;
     }
-    printf("%ld", cnt);
+    int total = 0;
+    for (int s = 0; s <= 27; s++) {
+        total += count[s] * count[s];
+    }
+    printf("%d", total);
     return 0;
 }
