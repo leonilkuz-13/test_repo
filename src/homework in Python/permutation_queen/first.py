5d285654ad01a9d52dd2063f1bc28665d9a01db5
Leonid <leonilfilippenkov@gmail.com>
2025-12-16 17:46:15 +0200

    adding rating and correcting function names

--- src/homework in Python/permutation_queen/first.py
+++ src/homework in Python/permutation_queen/first.py
@@ -4,7 +4,7 @@ from itertools import permutations
 N = int(input())
 
 
-def correction(permutation, N):
+def is_valid(permutation, N):
     for i in range(N):
         for j in range(i + 1, N):
             if abs(permutation[i] - permutation[j]) == abs(i - j):
@@ -16,7 +16,7 @@ count = 0
 
 
 for permutation in permutations(range(N)):
-    if not correction(permutation, N):
+    if not is_valid(permutation, N):
         continue
     count += 1
 
