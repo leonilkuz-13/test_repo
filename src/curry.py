aaa4ca41e6300075528ffe08d0a9bbc40c12db9b
Leonid <leonilfilippenkov@gmail.com>
2025-12-19 19:54:27 +0200

    adding a check for correct arity transfer

--- src/curry.py
+++ src/curry.py
@@ -3,14 +3,14 @@ def curry(func, arity):
         raise TypeError("arity is not integer")
     if arity < 0:
         raise ValueError("arity must be non-negative")
+    if arity > func.__code__.co_argcount:
+        raise ValueError("arity exceeds function's actual argument count")
 
     def curried(*args):
         if len(args) == arity:
             return func(*args)
         elif len(args) > arity:
-            raise ValueError(
-                "there are more arguments than arity"
-            )  # бесполезная проверка? или в комментариях к PR просили в uncurry?
+            raise ValueError("there are more arguments than arity")
         else:
 
             def inner(*new_args):
